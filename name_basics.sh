17071cf2710c1b3a85713463dbebbc10759f2ce7
John <john@johntelford.com>
2021-01-22 11:40:28 -0800

    checkpoint

--- name_basics.sh
+++ name_basics.sh
@@ -2,6 +2,7 @@
 
 echo "name_basics.sh"
 
+# command line parameters
 show_usage() {
     echo "name_basice.sh parameters:"
     echo "-by | --birthYear"
@@ -12,8 +13,7 @@ show_usage() {
     echo "-kft | --knownForTitles"
 }
 
-# process named arguments
-#echo "process named arguments"
+# named arguments
 while [ ! -z "$1" ]; do
     case "$1" in
     -by | --birthYear)
@@ -50,8 +50,7 @@ while [ ! -z "$1" ]; do
     shift
 done
 
-# Datasets
-imdb_dataset_attributes="/Volumes/Dev/imdb/imdb_dataset_attributes/"
+# datasets
 imdb_dateset_cache="/Volumes/Dev/imdb/imdb_dataset_cache/"
 imdb_dataset_in_files="/Volumes/Dev/imdb/imdb_dataset_files/"
 imdb_dataset_out_files="/Volumes/Dev/imdb/imdb_processing_out/"
@@ -75,22 +74,21 @@ shell_file=${imdb_dataset_out_files}$shell_name
 # title_principales_tconst.sh "nm0000078"
 # title_basics_primary_title.sh "tt0075213"
 
-# Cache file
+# cache file
 
 if test -f $cache_file; then
     echo "cashe_file $cache_name exists"
 else
-    echo "Creating $cache_name"
+    echo "creating $cache_file"
     touch ${cache_file}
-    #xsv slice ${fxl_file} --start 0 --end 0 >> ${cache_file}
+    xsv slice --start 0 --end 0 ${fxl_file} > ${cache_file}
+    #xsv cat rows ${cache_file} $cache_header
 fi
 
-# Construct xsv command stack
+# create xsv shell
 
 primaryName="'^*$primaryName$'"
 primaryProfession="'^*$primaryProfession$'"
-
-# Create xsv shell file
 
 # echo "xsv select 1-6 ${fxl_file} \
 # | xsv search --select 2 ${primaryName} \
@@ -102,35 +100,35 @@ echo "shell_file - ${shell_file}"
 # xsv select 1-50 dataset - to determine number of columns
 
 echo "xsv select  1-13 ${fxl_file} \
-    | xsv search  ${primaryName} \
-    | xsv search ${primaryProfession} \
-    | xsv slice  --no-headers  --start 1 " >${shell_file}
+| xsv search  ${primaryName} \
+| xsv search ${primaryProfession} \
+| xsv slice  --no-headers  --start 1 " >${shell_file}
 
-# Execute xsv shell
+# execute xsv shell
 
 chmod +x ${shell_file}
 ${shell_file} >${csv_file}
 
 cat ${csv_file}
 xsv count --no-headers ${csv_file}
-echo $search_count
-exit
 
-# Check if attributes found
+# FIXME  remove exit to execute cache
+
+#exit
+
+# check if attributes found
 if [[ -s ${csv_file} ]]; then
     # Attributes found
-    echo "attributes found"
-    xsv cat rows ${csv_file} --output ${cache_file}
+    echo "parameters found"
+    # FIXME xsv cat rows ${csv_file} --output ${cache_file}
     exit
 else
     # Attributes not found
-    echo "attributes not found"
+    echo "1 or more parameters not found"
     exit
 fi
 
-#  Process xsv command stack
-
-# Cache
+# cache
 # tmp cache
 #
 xsv cat rows ${imdb_dataset_out_files}name_basics.csv >${imdb_dateset_cache}name_basics_cache_temp.csv
