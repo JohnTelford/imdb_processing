6d673bb8edc010294943098c220c9861b7fdbda0
John <john@johntelford.com>
2021-01-31 18:20:40 -0800

    checkpoint name.basic rg working

--- functions/name_basics_param.sh
+++ functions/name_basics_param.sh
@@ -39,3 +39,27 @@
         shift
     done
 }
+
+# datasets
+imdb_dateset_cache="/Volumes/Dev/imdb/imdb_dataset_cache/"
+imdb_dataset_in_files="/Volumes/Dev/imdb/imdb_dataset_files/"
+imdb_dataset_out_files="/Volumes/Dev/imdb/imdb_processing_out/"
+
+# Note: all fxl_ file have been ln -s 
+
+imdb_dataset_in="$imdb_dataset_in_files$fxl_name"
+imdb_dataset_out="$imdb_dataset_out_files$fxl_name"
+
+cache_name="name.basics_cache.csv"
+cache_file=${imdb_dateset_cache}$cache_name
+cache_name_temp="name.basics_cache_temp.csv"
+cache_file_temp=${imdb_dateset_cache}$cache_name_temp
+
+csv_name="name.basics_csv"
+csv_file=${imdb_dataset_out_files}$csv_name
+
+fxl_file=${imdb_dataset_in}
+# FIXME
+shell_name="name.basics_shell.sh"
+shell_file=${imdb_dataset_out_files}$shell_name
+
--- imdb_scripts/imdb_name.basics_to_title.basics.sh
+++ imdb_scripts/imdb_name.basics_to_title.basics.sh
@@ -45,17 +45,17 @@ while [ ! -z "$1" ]; do
     -fn | --firstName)
         shift
         firstName=$1
-        echo "--primaryName $firstName"
+        echo "--firstName $firstName"
         ;;
     -ln | --lastName)
         shift
         lastName=$1
-        echo "--primaryName $lastName"
+        echo "--lastName $lastName"
         ;;
-    -p | --primaryProfession)
+    -p | --Profession)
         shift
-        primaryProfession=$1
-        echo "--primaryProfession $primaryProfession"
+        Profession=$1
+        echo "--Profession"
         ;;
     *)
         echo "parameter error"
@@ -65,4 +65,91 @@ while [ ! -z "$1" ]; do
     esac
     shift
 done
-git sta
+
+echo $firstName
+echo $lastName
+echo $Profession
+
+#printf "%s %s," ${firstName} ${lastName} > searchName
+#echo "${firstName} ${lastName}," > searchName
+searchName="${firstName} ${lastName},"
+echo "$searchName"
+
+
+# datasets
+imdb_dateset_cache="/Volumes/Dev/imdb/imdb_dataset_cache/"
+imdb_dataset_in_files="/Volumes/Dev/imdb/imdb_dataset_files/"
+imdb_dataset_out_files="/Volumes/Dev/imdb/imdb_processing_out/"
+
+# HACK  IMDb dataset files have been `ln -s` 
+#fxl_name="fxl_name.basics.csv"
+name_basics=name.basics.csv
+
+
+
+imdb_dataset_in="$imdb_dataset_in_files$name_basics"
+name_basics_in=${name_basics}
+imdb_dataset_out="$imdb_dataset_out_files$name_basics"
+
+cache_name="name.basics_cache.csv"
+cache_file=${imdb_dateset_cache}$cache_name
+cache_name_temp="name.basics_cache_temp.csv"
+cache_file_temp=${imdb_dateset_cache}$cache_name_temp
+
+csv_name="name.basics_csv"
+csv_file=${imdb_dataset_out_files}$csv_name
+
+shell_name="name.basics_shell.sh"
+shell_file=${imdb_dataset_out_files}$shell_name
+
+if [[  -s $cache_file ]]; then
+    cf=true
+    echo "cache_file $cache_file exists"
+else
+    cf=false
+    echo "creating $cache_file"
+    touch ${cache_file}
+    # create cache_file header
+    xsv slice --start 0 --end 0  ${name_basics_in} > ${cache_file}
+fi
+
+#if $cf ; then
+
+    echo "cf_true"
+    # # search cache_file. If not found search fxl_file
+    # echo "xsv select  1-13 ${cache_file} \
+    # | xsv search  ${primaryName} \
+    # | xsv search ${primaryProfession} \
+    # | xsv slice  --no-headers  --start 1 " >${shell_file}
+    # # execute xsv shell
+    # chmod +x ${shell_file}
+    # ${shell_file} >${csv_file}
+
+rg "$searchName" name.basics.csv | gawk -F, '$0 ~ /actor/ ' >> ${cache_file}
+echo $?
+exit
+
+    # # cache_file search failed
+    # if [[ ! -s ${csv_file} ]]; then
+    #     cf=false
+    #     echo "xsv select  1-13 ${fxl_file} \
+    #     | xsv search  ${primaryName} \
+    #     | xsv search ${primaryProfession} \
+    #     | xsv slice  --no-headers  --start 1 " >${shell_file}
+    #     # execute xsv shell
+    #     chmod +x ${shell_file}
+    #     ${shell_file} >${csv_file}
+    # fi
+#else
+    #echo "cf_false"
+    # echo "xsv select  1-13 ${fxl_file} \
+    # | xsv search  ${primaryName} \
+    # | xsv search ${primaryPr"ofession} \
+    # | xsv slice  --no-headers  --start 1 " > ${shell_file}
+    # # execute xsv shell
+    # chmod +x ${shell_file}
+    # ${shell_file} >${csv_file}
+
+#rg "John Wayne,\d+," name.basics.csv | gawk -F, '$0 ~ /actor/ { print $0 }' >> ${cache_file}
+#fi
+
